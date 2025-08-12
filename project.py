90c0298162d1c2224aa6bc9d3b737f9ff3639c24
Bob <168981911+RaimondoSoleglio@users.noreply.github.com>
2025-08-12 20:10:49 +0100

    first commit

---
+++ project.py
@@ -0,0 +1,23 @@
+
+COLOURS = ['up', 'right', 'down', 'left']
+
+def main():
+    
+    score = 0
+    
+    sequence = []
+    
+    
+    while True:
+        
+        # computer adds a colour to the sequence
+        
+        # computer shows the sequence
+        
+        # player pick a colour
+        
+        # if wrong ---> game over EXIT LOOP
+        
+        # if right, the loop continues ---> score += 1
+        
+        
