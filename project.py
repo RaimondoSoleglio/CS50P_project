97d83f5fa52e0687de65b1cc5d3174021e390aca
Bob <168981911+RaimondoSoleglio@users.noreply.github.com>
2025-08-25 00:11:14 +0100

    show seq to be implemented

--- project.py
+++ project.py
@@ -1,6 +1,23 @@
 import random
 import pygame
 
+# define 4 colours
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# off status of colours
+RED_OFF = (100, 0, 0)
+GREEN_OFF = (0, 100, 0)
+BLUE_OFF = (0, 0, 100)
+YELLOW_OFF = (100, 100, 0)
+
+# define rects for 4 buttons (left, top, w, h)
+top = pygame.Rect(220, 40, 160, 160)
+left = pygame.Rect(40, 220, 160, 160)
+bottom = pygame.Rect(220, 400, 160, 160)
+right = pygame.Rect(400, 220, 160, 160)
 
 COLOURS = ['up', 'left', 'down', 'right']
 
@@ -35,27 +52,38 @@ def main():
         colour_seq(sequence)
         
         # computer shows the sequence
-        show_seq(sequence)
+        show_seq(sequence, screen)
         
         # player pick a colour
-        answer = player_action(sequence) # returns wrong or right
+        # answer = player_action(sequence) # returns wrong or right
         
         # if wrong ---> game over EXIT LOOP
-        if answer == "wrong":
-            break
+        # if answer == "wrong":
+        #     break
         
         # if right, the loop continues ---> score += 1
-        score += 1
+        # score += 1
         
-        gameover()
-        
+
         # --- DRAWING CODE ---
-        # starting with total black
         
+        # starting with total black
         screen.fill((0, 0, 0))
+        
+        # rects
+        pygame.draw.rect(screen, RED, top, border_radius = 30)
+        pygame.draw.rect(screen, GREEN, left, border_radius = 30)
+        pygame.draw.rect(screen, BLUE, bottom, border_radius = 30)
+        pygame.draw.rect(screen, YELLOW, right, border_radius = 30)
+        
         
         # at the end we need updating he screen with all the drawing in this loop iteration
         pygame.display.flip()
+        
+        
+    # we are here once the 'while' loop is finished
+    print("Game over! Your score was:", score)
+    pygame.quit()
         
     
 def colour_seq(sequence):
@@ -70,75 +98,58 @@ def colour_seq(sequence):
     
 def show_seq(sequence):
     
-    # for loop to show the colour one by one for 1 sec
-    for colour in sequence:
-        print(colour)  
-        time.sleep(1)
-        os.system('clear')    
+    # mapping COLOURS with rects and properties
+    rect_map = {
+        'up': {'rect': top, 'on': RED, 'off': RED_OFF},
+        'left': {'rect': left, 'on': GREEN, 'off': GREEN_OFF},
+        'down': {'rect': bottom, 'on': BLUE, 'off': BLUE_OFF},
+        'right': {'rect': right, 'on': YELLOW, 'off': YELLOW_OFF},
+    }        
+    
+    
+    
+# def player_choice():
+    
+#     choice = None
+    
+#     def release_key(key):
         
-    
-    
-def player_choice():
-    
-    choice = None
-    
-    def release_key(key):
+#         nonlocal choice
         
-        nonlocal choice
+#         if key == Key.up:
+#             choice = 'up'
+#         elif key == Key.down:
+#             choice = 'down'
+#         elif key == Key.left:
+#             choice = 'left'
+#         elif key == Key.right:
+#             choice = 'right'
         
-        if key == Key.up:
-            choice = 'up'
-        elif key == Key.down:
-            choice = 'down'
-        elif key == Key.left:
-            choice = 'left'
-        elif key == Key.right:
-            choice = 'right'
+#         try:
+#             if key.char == 'w':
+#                 choice = 'up'
+#             elif key.char == 'a':
+#                 choice = 'left'
+#             elif key.char == 's':
+#                 choice = 'down'
+#             elif key.char == 'd':
+#                 choice = 'right'
+#         except AttributeError:
+#             pass
+    
         
-        try:
-            if key.char == 'w':
-                choice = 'up'
-            elif key.char == 'a':
-                choice = 'left'
-            elif key.char == 's':
-                choice = 'down'
-            elif key.char == 'd':
-                choice = 'right'
-        except AttributeError:
-            pass
+#     return choice
     
     
-    with Listener(on_release = release_key) as l:
-        l.join()
-        
-    return choice
+# def player_action(sequence):
     
+#     # player gets asked "What's the sequence?"
+#     print("What's the sequence? [Use WASD or arrows]")
     
-def player_action(sequence):
-    
-    # player gets asked "What's the sequence?"
-    print("What's the sequence? [Use WASD or arrows]")
-    
-    for colour in sequence:
-        if colour != player_choice():
-            return "wrong"
+#     for colour in sequence:
+#         if colour != player_choice():
+#             return "wrong"
          
-         
-def gameover():
-    
-    # We show the score and ask if the player wants to play another game
-    print("Your score is", score)
-    new_game = input("Do you want to play another game? Y/n").lower()
-    
-    # Here it could be interesting to apply restrictions about what can be replied (and apply some of the lessons we studied)
-    if new_game == "y":
-        main()
-        
-    elif new_game == "n":
-        print("OK, see you next time!")
-        exit()   # can we print INSIDE exit function?? can't remember
-        
-        
 
         
 if __name__ == "__main__":
